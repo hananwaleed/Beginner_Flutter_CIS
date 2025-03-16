be628660450651ec084693ffc4a24fe377851d40
Hanan waleed <waleedhanan81@gmail.com>
2025-03-16 02:01:17 +0200

    Saved changes

---
+++ TaskWeek7/week_7/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.white,),
+      ),
+    );
+  }
+}
