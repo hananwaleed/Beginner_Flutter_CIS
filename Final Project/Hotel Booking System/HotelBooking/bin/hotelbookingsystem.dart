b341eddbda12d2154f7e2386465c8ab50788b282
Hanan waleed <waleedhanan81@gmail.com>
2025-03-25 05:59:44 +0200

    Created Project

---
+++ Final Project/Hotel Booking System/HotelBooking/bin/hotelbookingsystem.dart
@@ -0,0 +1,5 @@
+import 'package:hotelbookingsystem/hotelbookingsystem.dart' as hotelbookingsystem;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hotelbookingsystem.calculate()}!');
+}
